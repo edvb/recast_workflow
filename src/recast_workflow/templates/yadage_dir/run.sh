a12df57e6c03722827971993c61044a65934766d
Ed van Bruggen <edvb@uw.edu>
2020-10-31 21:54:33 -0700

    run.sh: Don't give warning when removing workdir

--- src/recast_workflow/templates/yadage_dir/run.sh
+++ src/recast_workflow/templates/yadage_dir/run.sh
@@ -5,5 +5,5 @@ DIR=$(dirname "$0")
 # workflow to run, defaults to workflow.yml if no argument is given
 WORKFLOW=${1:-$DIR/workflows/workflow.yml}
 
-rm -r $DIR/workdir
+rm -rf $DIR/workdir
 yadage-run $DIR/workdir $WORKFLOW $DIR/inputs/input.yml -d initdir=$PWD/$DIR/inputs
