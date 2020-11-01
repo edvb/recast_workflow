3e70d5f66799ea6d596493a63f3c912d2c35d56c
Ed van Bruggen <edvb@uw.edu>
2020-10-31 21:52:21 -0700

    Make run.sh executable and from any directory

--- src/recast_workflow/templates/yadage_dir/run.sh
+++ src/recast_workflow/templates/yadage_dir/run.sh
@@ -1,3 +1,9 @@
-rm -r workdir
-WORKFLOW=${1:-workflows/workflow.yml}
-yadage-run workdir $WORKFLOW inputs/input.yml -d initdir=$PWD/inputs
+#!/bin/sh
+
+# get directory of workflow
+DIR=$(dirname "$0")
+# workflow to run, defaults to workflow.yml if no argument is given
+WORKFLOW=${1:-$DIR/workflows/workflow.yml}
+
+rm -r $DIR/workdir
+yadage-run $DIR/workdir $WORKFLOW $DIR/inputs/input.yml -d initdir=$PWD/$DIR/inputs
