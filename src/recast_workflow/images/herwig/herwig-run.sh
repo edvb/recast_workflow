13a51765e300f83a50c19b169f44cef4f6c9f9b3
Ed van Bruggen <edvb@uw.edu>
2021-05-25 00:55:55 -0700

    Fix herwig subworkflow

--- src/recast_workflow/images/herwig/herwig-run.sh
+++ src/recast_workflow/images/herwig/herwig-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 INFILE="$1"
-RUNFILE="${1%.in}.run"
+RUNFILE=$(basename "${1%.in}.run")
 EVENTS=${2:-10000}
 
-/herwig/bin/Herwig read $INFILE
-/herwig/bin/Herwig run $RUNFILE -N $EVENTS
+/herwig/bin/Herwig read "$INFILE"
+/herwig/bin/Herwig run "$RUNFILE" -N $EVENTS
